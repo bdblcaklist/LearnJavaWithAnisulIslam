1c583bb412f271d72424507f1bdf6941b1678ea6
bdblacklist <motiurny@gmail.com>
2024-06-06 12:31:27 -0400

    Escape Sequence

---
+++ src/EscapeSequence.java
@@ -0,0 +1,34 @@
+public class EscapeSequence {
+    public static void main(String[] args) {
+
+        System.out.println(" Armaan Rahman\n 01722016633 \n Masters of Busuiness Management");
+        //System.out.println("01722016633");
+        //System.out.println("Masters of Business management");
+
+
+        //Escape sequence Special character followed by backslash
+        //iT IS A
+        /*
+        \b      backspace
+        \t      tab
+        \n      New Line
+        \r      carriage return. ()
+        \f      form feed
+        \'      Inserts a single quote
+        \ "     Inserts a double quote
+        \\      Inserts a backl slash
+         */
+
+        System.out.println("2 \t 2");
+        System.out.println("3 \t 4");
+        System.out.println("3333 \t 4333 \t 456789 \t 98765");
+        System.out.println("3333 \t 4333 \t 456789 \t 98765");
+        System.out.println("3333 \t 4333 \t 456789 \t 98765");
+        System.out.println("3333 \t 4333 \t 456789 \t 98765");
+        System.out.println("3333 \t 4333 \t 456789 \t 98765");
+        System.out.println("3333 \t 4333 \t 456789 \t 98765");
+        System.out.println("3333 \t 4333 \t 456789 \t 98765");
+
+
+    }
+}
